8fda12221185e476d2dbc8a077249e18700174ef
emilyma53 <52176221+emilyma53@users.noreply.github.com>
2019-08-07 16:05:18 -0700

    Add files via upload
    datascience module

---
+++ datasciencemaster/Makefile
@@ -0,0 +1,42 @@
+.PHONY: help docs serve_docs install test deploy_docs
+
+DOCS_DIR = docs
+GH_REMOTE = $(shell git remote -v | grep "origin.*(fetch)" | grep -E "\S+\\.\S+" -o)
+DEPLOY_DOCS_MESSAGE = Build docs
+
+help:
+	@echo "Please use 'make <target>' where <target> is one of:"
+	@echo "  install     to install the datascience package locally"
+	@echo "  test        to run the tests"
+	@echo "  docs        to build the docs"
+	@echo "  clean_docs  to remove the doc files"
+	@echo "  serve_docs  to serve the docs from a local Python server"
+	@echo "  deploy_docs to deploy the docs to Github Pages"
+
+install:
+	python setup.py develop
+
+test:
+	python setup.py test
+
+docs:
+	cd $(DOCS_DIR) ; make html
+
+clean_docs:
+	cd $(DOCS_DIR) ; make clean
+
+serve_docs:
+	cd $(DOCS_DIR)/_build/html ; python -m http.server
+
+deploy_docs:
+	rm -rf doc_build
+
+	git clone --quiet --branch=gh-pages $(GH_REMOTE) doc_build
+	cp -r docs/_build/html/* doc_build
+
+	cd doc_build && \
+		git add -A && \
+		git commit -m "$(DEPLOY_DOCS_MESSAGE)" && \
+		git push -f $(GH_REMOTE) gh-pages
+
+	rm -rf doc_build
---
+++ datasciencemaster/tests/test_maps.py
@@ -0,0 +1,167 @@
+import doctest
+import json
+import pytest
+
+import datascience as ds
+from datascience import maps
+
+
+@pytest.fixture(scope='function')
+def states():
+    """Read a map of US states."""
+    return ds.Map.read_geojson('tests/us-states.json')
+
+
+############
+# Doctests #
+############
+
+
+def test_doctests():
+    results = doctest.testmod(maps, optionflags=doctest.NORMALIZE_WHITESPACE)
+    assert results.failed == 0
+
+
+############
+# Overview #
+############
+
+
+def test_draw_map(states):
+    """ Tests that draw_map returns HTML """
+    states.show()
+
+
+def test_setup_map():
+    """ Tests that passing kwargs doesn't error. """
+    kwargs = {
+        'tiles': 'Stamen Toner',
+        'zoom_start': 17,
+        'width': 960,
+        'height': 500,
+        'features': [],
+    }
+    ds.Map(**kwargs).show()
+
+
+def test_map_marker_and_region(states):
+    """ Tests that a Map can contain a Marker and/or Region. """
+    marker = ds.Marker(51.514, -0.132)
+    ds.Map(marker).show()
+    ds.Map([marker]).show()
+    region = states['CA']
+    ds.Map(region).show()
+    ds.Map([region]).show()
+    ds.Map([marker, region]).show()
+
+
+##########
+# ds.Marker #
+##########
+
+
+def test_marker_html():
+    """ Tests that a Marker can be rendered. """
+    ds.Marker(51.514, -0.132).show()
+
+
+def test_marker_map():
+    """ Tests that Marker.map generates a map """
+    lats = [51, 52, 53]
+    lons = [-1, -2, -3]
+    labels = ['A', 'B', 'C']
+    colors = ['blue', 'red', 'green']
+    ds.Marker.map(lats, lons).show()
+    ds.Marker.map(lats, lons, labels).show()
+    ds.Marker.map(lats, lons, labels, colors).show()
+    ds.Marker.map(lats, lons, colors=colors).show()
+
+
+def test_marker_map_table():
+    """ Tests that Marker.map_table generates a map """
+    lats = [51, 52, 53]
+    lons = [-1, -2, -3]
+    labels = ['A', 'B', 'C']
+    t = ds.Table().with_columns('A', lats, 'B', lons, 'C', labels)
+    ds.Marker.map_table(t).show()
+    colors = ['red', 'green', 'yellow']
+    t['colors'] = colors
+    ds.Marker.map_table(t).show()
+
+
+def test_circle_html():
+    """ Tests that a Circle can be rendered. """
+    ds.Circle(51.514, -0.132).show()
+
+
+def test_circle_map():
+    """ Tests that Circle.map generates a map """
+    lats = [51, 52, 53]
+    lons = [-1, -2, -3]
+    labels = ['A', 'B', 'C']
+    ds.Circle.map(lats, lons).show()
+    ds.Circle.map(lats, lons, labels).show()
+
+
+##########
+# Region #
+##########
+
+
+def test_region_html(states):
+    states['CA'].show()
+
+
+def test_geojson(states):
+    """ Tests that geojson returns the original US States data """
+    data = json.load(open('tests/us-states.json', 'r'))
+    geo = states.geojson()
+    assert data == geo, '{}\n{}'.format(data, geo)
+
+
+##########
+# Bounds #
+##########
+
+
+def test_bounds():
+    """ Tests that generated bounds are correct """
+    points = [ds.Marker(0, 0), ds.Marker(-89.9, 180), ds.Marker(90, -180)]
+    bounds = ds.Map(points)._autobounds()
+    assert bounds['max_lat'] == 90
+    assert bounds['min_lat'] == -89.9
+    assert bounds['max_lon'] == 180
+    assert bounds['min_lon'] == -180
+
+
+def test_bounds_limits():
+    """ Tests that too-large lats and lons are truncated to real bounds. """
+    points = [ds.Marker(0, 0), ds.Marker(-190, 280), ds.Marker(190, -280)]
+    bounds = ds.Map(points)._autobounds()
+    assert bounds['max_lat'] == 90
+    assert bounds['min_lat'] == -90
+    assert bounds['max_lon'] == 180
+    assert bounds['min_lon'] == -180
+
+
+#########
+# Color #
+#########
+
+
+def test_color_table(states):
+    """ Tests that color can take a Table. """
+    data = ds.Table.read_table('tests/us-unemployment.csv')
+    states.color(data).show()
+
+
+def test_color_dict(states):
+    """ Tests that color can take a dict. """
+    data = ds.Table.read_table('tests/us-unemployment.csv')
+    states.color(dict(zip(*data.columns))).show()
+
+
+def test_color_values_and_ids(states):
+    """ Tests that color can take values and ids. """
+    data = ds.Table.read_table('tests/us-unemployment.csv')
+    states.color(data['Unemployment'], data['State']).show()
